3b12d3ca2d789be75491af5249705674e3c04b94
Ishita Choudhary <ishitachoudhary270900@gmail.com>
2022-02-05 22:39:52 +0530

    added .env and connected mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(request,response){
-    response.send('Hello');
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
 });
 
-app.listen(3000, function(){
-    console.log('Server started on port 3000');
+app.get('/', function(request,response){
+    response.send("<h1>Hello</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on port ${port}`);
 });
