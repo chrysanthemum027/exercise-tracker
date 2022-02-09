46983c2e048609f00982cfab17e778db77e14d5e
Ishita Choudhary <ishitachoudhary270900@gmail.com>
2022-02-09 15:04:42 +0530

    created components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateExercises extends Component{
+    render(){
+        return(
+            <div>
+                <p>This is the Create Exercises Component!</p>
+            </div>
+        )
+    }
+}
