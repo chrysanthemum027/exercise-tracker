7cea68be04eac7ce3f7175390b70c56bd03d5da7
Ishita Choudhary <ishitachoudhary270900@gmail.com>
2022-02-10 23:50:33 +0530

    Update create-user.component.js

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 
 export default class CreateUser extends Component{
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        }
+
+        console.log(user)
+
+        this.setState({
+            username: ''
+        })
+    }
     render(){
         return(
-            <div>
-                <p>This is the Create User Component!</p>
-            </div>
+         <div>
+             Hello world
+         </div>   
         )
     }
 }
