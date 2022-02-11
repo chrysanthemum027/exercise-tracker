c5ed1725f277cf17ec323a343d0c3794c2acd82d
Ishita Choudhary <ishitachoudhary270900@gmail.com>
2022-02-11 16:00:45 +0530

    connected create-user component to database

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component{
     constructor(props) {
@@ -27,6 +28,9 @@ export default class CreateUser extends Component{
 
         console.log(user)
 
+        axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data))
+
         this.setState({
             username: ''
         })
@@ -34,7 +38,21 @@ export default class CreateUser extends Component{
     render(){
         return(
          <div>
-             Hello world
+             <h3>Create New User</h3>
+             <form onSubmit={this.onSubmit}>
+                 <div className='form-group'>
+                     <label>Username: </label>
+                     <input type='text'
+                     required
+                     className='form-control'
+                     value={this.state.username}
+                     onChange={this.onChangeUsername}
+                     />
+                 </div>
+                 <div className='form-group'>
+                     <input type='submit' value='Create User' className='btn btn-primary'/>
+                 </div>
+             </form>
          </div>   
         )
     }
